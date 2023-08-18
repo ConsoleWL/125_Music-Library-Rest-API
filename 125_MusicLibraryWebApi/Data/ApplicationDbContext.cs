91675c23d88eaf41976d9820d6b51c1b8af6267b
Nikita Obergan <Obergannikita@gmail.com>
2023-08-17 20:33:33 -0400

    Add packages, add migration, add Connection string, add Model

---
+++ 125_MusicLibraryWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _125_MusicLibraryWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _125_MusicLibraryWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ 125_MusicLibraryWebApi/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _125_MusicLibraryWebApi.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Genre { get; set; }
+    }
+}
